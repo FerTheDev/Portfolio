aa479c818797261f5e27272e9c377062b474bdd9
FerTheDev <fer_k_dev@outlook.com>
2026-03-16 19:06:49 -0300

    Initial project structure

---
+++ PortfolioFer/PortfolioFer.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace PortfolioFer.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ PortfolioFer/PortfolioFer/Features/HelloWorld/Controllers/HelloWorldController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioFer.Features.HelloWorld.Controllers;
+
+[ApiController]
+[Route("api/v1/hello-world")]
+public class HelloWorldController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Hello World");
+    }
+}
