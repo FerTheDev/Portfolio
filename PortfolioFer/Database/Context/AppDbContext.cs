7dc630c408867600c643acba592835fbdc6509ae
FerTheDev <fer_k_dev@outlook.com>
2026-04-16 16:12:34 -0300

    database conected

---
+++ PortfolioFer/Database/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PortfolioFer.Features.Profile.Entities;
+using PortfolioFer.Features.Projects.Entities;
+using System.Collections.Generic;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace PortfolioFer.Database
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Profile> Profiles { get; set; }
+    }
+}
